b20731579e797e7087c8d10edb8d23f06bf8ffee
ddeloach <deloachaero@gmail.com>
2018-04-04 09:22:09 -0700

    Added global exception handler class
    RFC7807GlobalExceptionHandler to be registered as a service in WebApiConfig. See class comments in source file.

---
+++ src/RFC7807ErrorMessages/RFC7807GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace DeLoachAero.WebApi
+{
+    /// <summary>
+    /// Global exception handler to convert all exceptions into RFC7807 messages as the response
+    /// </summary>
+    /// <remarks>
+    /// To register, in WebApiConfig.cs:
+    /// <code>
+    /// config.Services.Replace(typeof(IExceptionHandler), new RFC7807GlobalExceptionHandler());
+    /// </code>
+    /// </remarks>
+    public class RFC7807GlobalExceptionHandler : ExceptionHandler
+    {
+        public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            // nothing we can do if the context is not present
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            // verify this exception should be handled at all; exit if not
+            if (!ShouldHandle(context))
+            {
+                return Task.FromResult(0);
+            }
+
+            // convert the exception into an RFC7807Exception if required, then create an IHttpActionResult from it
+            var ex = context.Exception as RFC7807Exception;
+            if (ex == null)
+                ex = new RFC7807Exception(context.Exception, context.Request.RequestUri);
+
+            context.Result = context.Request.CreateRFC7807ProblemActionResult(ex.ProblemDetail);
+
+            return Task.FromResult(0);
+        }
+
+    }
+}
