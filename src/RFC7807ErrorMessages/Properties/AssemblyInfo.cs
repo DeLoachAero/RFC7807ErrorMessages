c22d1d8b4c65c0651c9ddb337b4401bbb1017692
DDeLoachWG <darren.deloach@watchguard.com>
2021-01-21 10:39:38 -0800

    Update Assembly and file versions

--- src/RFC7807ErrorMessages/Properties/AssemblyInfo.cs
+++ src/RFC7807ErrorMessages/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RFC7807ErrorMessages")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Support for RFC7807 in ASP.NET Web API 2.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RFC7807ErrorMessages")]
-[assembly: AssemblyCopyright("Copyright © 2018 Darren DeLoach")]
+[assembly: AssemblyCopyright("Copyright © 2018,2021 Darren DeLoach")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
